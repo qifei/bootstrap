668d429ba471bc93eadf9fbeffaf292a122b14f3
GeoSot <geo.sotis@gmail.com>
2021-03-03 02:33:02 +0200

    revamp scrollspy to use IntersectionObserver

--- js/tests/karma.conf.js
+++ js/tests/karma.conf.js
@@ -61,18 +61,19 @@ const conf = {
   autoWatch: false,
   singleRun: true,
   concurrency: Number.POSITIVE_INFINITY,
+  restartOnFileChange: true,
   client: {
     clearContext: false
   },
   files: [
     'node_modules/hammer-simulator/index.js',
     {
-      pattern: 'js/tests/unit/**/!(jquery).spec.js',
+      pattern: 'js/tests/unit/scrollspy.spec.js',
       watched: !BROWSERSTACK
     }
   ],
   preprocessors: {
-    'js/tests/unit/**/*.spec.js': ['rollup']
+    'js/tests/unit/scrollspy.spec.js': ['rollup']
   },
   rollupPreprocessor: {
     plugins: [
